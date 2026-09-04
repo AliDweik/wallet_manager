36d356e38071a41d91eae6e53df557e2da8adf48
Ali Dweik <ali2dweik3@gmail.com>
2026-09-04 17:40:26 +0300

    Adding App Data Models

--- lib/utlils/constants.dart
+++ lib/utlils/constants.dart
@@ -2,8 +2,8 @@ class AppConstants {
   static const String activeDataFile = 'current_month.json';
   static const String archivePrefix = 'archive_';
 
-  static const double defaultSalaryAmount = 370;
-  static const int defaultSalaryDay = 27;
+  static const double defaultSalaryAmount = 0;
+  static const int defaultSalaryDay = 28;
   static const String defaultSalaryAccount = 'visa';
 
   static String getArchiveFileName(DateTime date) {
